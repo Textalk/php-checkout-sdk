43c5091030a6213add25249620d1f1f151f8ac25
Mikael <mikael.kajsjo@textalk.se>
2018-02-08 13:03:47 +0100

    namespace fix

--- sdk/config.php
+++ sdk/config.php
@@ -1,13 +1,14 @@
 <?php
-namespace WasaKredit\Sdk;
+namespace WasaKredit\Sdk {
 
-function wasa_config($key = '')
-{
-    $wasa_configuration = [
-        'base_url' => 'https://b2b.services.wasakredit.se',
-        'access_token_url' => 'https://b2b.services.wasakredit.se/auth/connect/token'
-    ];
+    function wasa_config($key = '')
+    {
+        $wasa_configuration = [
+            'base_url' => 'https://b2b.services.wasakredit.se',
+            'access_token_url' => 'https://b2b.services.wasakredit.se/auth/connect/token'
+        ];
 
-    return isset($wasa_configuration[$key]) ? $wasa_configuration[$key] : null;
+        return isset($wasa_configuration[$key]) ? $wasa_configuration[$key] : null;
+    }
+
 }
-?>
